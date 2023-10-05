7463135e741af36631d4cdd3e4d4a47b5a3aed4b
LOUISENZR <louisanaza@gmail.com>
2023-10-05 09:40:28 +0700

    second commit

--- Pemilihan2percobaan1_16.java
+++ Pemilihan2percobaan1_16.java
@@ -7,7 +7,7 @@ public static void main(String[] args) {
     Scanner sc16 = new Scanner(System.in);
     System.out.print("Masukan tahun: ");
     int tahun = sc16.nextInt();
-    if((tahun % 4) == 0){
+    if((tahun % 400) == 0){
         if ((tahun % 100) != 0)
             System.out.println("Tahun kabisat");
     } 
